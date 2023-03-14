e34251312024a9f4d7326696c965e65b0e46afab
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-03-14 19:04:28 -0300

    desenvolvimento do model para criação de usuário

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -8,3 +8,16 @@ export interface NewProduct {
   name: string;
   amount: string;
 }
+
+type Password = string | number;
+
+export interface NewUser {
+  username: string;
+  vocation: string;
+  level: number;
+  password: Password;
+}
+
+export interface User extends NewUser{
+  id: number;
+}
---
+++ src/models/users.model.ts
@@ -0,0 +1,18 @@
+import { ResultSetHeader } from 'mysql2';
+import connection from './connection';
+import { NewUser, User } from '../interfaces';
+
+const createUser = async (user:NewUser) : Promise<NewUser> => {
+    const { username, vocation, level, password } = user;
+  
+    const [result] = await connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.users (username, vocation, level, password) VALUES (?, ?, ?, ?)',
+      [username, vocation, level, password],
+    );
+    const { insertId: id } = result;
+    const newUser: User = { id, ...user };
+    return newUser;
+  };
+
+const userModel = { createUser };
+export default userModel;
