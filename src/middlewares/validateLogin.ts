7fb814162912804ed97f8d0c95498fdce20d363e
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-03-15 16:06:04 -0300

    desenvolvimento de middleware de validação para a rota login

---
+++ src/middlewares/validateLogin.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from 'express';
+
+const validateLogin = (req: Request, res: Response, next: NextFunction) => {
+  const { username, password } = req.body;
+  if (!username) {
+    return res.status(400).json({ message: '"username" is required' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: '"password" is required' });
+  }
+  next();
+};
+
+const loginMiddleware = { validateLogin };
+export default loginMiddleware;
---
+++ src/routes/login.routes.ts
@@ -0,0 +1,3 @@
+import { Router } from 'express';
+import loginController from '../controllers/login.controller';
+import validateLogin  from '../middlewares/validateLogin';
